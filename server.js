9b0bd640f9172dbb2b7080f94c12f59b6c7e1b0b
Glitch (aquamarine-trumpet) <none>
2018-09-25 04:53:04 +0000

    🏂🐠 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,58 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date_string", function (req, res) {
+  console.log(req.params);
+  let return_date;
+  let date = req.params.date_string;
+  
+  if (date === "") {
+    return_date = new Date();
+  }
+  else if (!isNaN(date)) {
+    let date_int = parseInt(date);
+    return_date = new Date(date_int);
+  }
+  else {
+   return_date = new Date(date); 
+  }
+  
+  console.log(return_date);
+  
+  if (return_date instanceof Date && !isNaN(return_date.valueOf())) {
+    res.json({"unix": return_date.getTime(), "utc" : return_date.toUTCString() });
+  }
+  else {
+    res.json({"error" : "Invalid Date" });
+  }
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
